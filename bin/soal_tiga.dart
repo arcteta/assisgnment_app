c431824dc2f17616f73e7845df0c4bb08282db92
arcteta <ridhoadha117@gmail.com>
2024-01-24 06:04:25 +0700

    update: solved no 3

--- bin/soal_tiga.dart
+++ bin/soal_tiga.dart
@@ -1,11 +1,24 @@
 void main() {
-  List<int> arr = [1, 2, 3, 4, 5, 6, 7, 8];
+  List<int> arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
 
-  print(arr);
+  List<int> result = printArrPrime(arr);
+  print(result);
 }
 
-List<int> checkPrime(List<int> input) {
+List<int> printArrPrime(List<int> input) {
+  List<int> theNumber = [];
+  for (int e in input) {
+    if (checkPrime(e)) {
+      theNumber.add(e);
+    }
+  }
+  return theNumber;
+}
 
-
-
+bool checkPrime(int number) {
+  if (number <= 1) return false; // Prime number is > 1
+  for (int i = 2; i < number; i++) {
+    if (number % i == 0) return false;
+  }
+  return true;
 }
