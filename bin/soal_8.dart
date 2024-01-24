1b3319165be5f0f5c227ca9ffe7550698371d6fc
arcteta <ridhoadha117@gmail.com>
2024-01-24 09:04:54 +0700

    commit solved no 8

--- bin/soal_8.dart
+++ bin/soal_8.dart
@@ -1,12 +1,19 @@
 void main() {
-  String yourText1 = 'ular';
-  String yourText2 = 'alur';
+  String yourText1 = 'william shakespeare';
+  String yourText2 = 'i am a weakish speller';
 
-  isAnagram(yourText1, yourText2);
-  // print(isAnagram(yourText1, yourText2));
+  // isAnagram(yourText1, yourText2);
+  print(isAnagram(yourText1, yourText2));
 }
 
-void isAnagram(String text1, String text2) {
-  List<String> sorted1 = text1.split("")..sort();
-  List<String> sorted2 = text2.split("")..sort();
+bool isAnagram(String text1, String text2) {
+  List<String> sorted1 = text1.toLowerCase().replaceAll(" ","").split("")..sort();
+  List<String> sorted2 = text2.toLowerCase().replaceAll(" ","").split("")..sort();
+
+  for (var i = 0; i < sorted1.length; i++) {
+    if (sorted1[i] != sorted2[i]) {
+      return false;
+    }
+  }
+  return true;
 }
