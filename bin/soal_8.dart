765c1c10e0ba15237390fdf0306c90fd614ac170
arcteta <ridhoadha117@gmail.com>
2024-01-24 08:02:04 +0700

    change: soal_8.dart

--- bin/soal_8.dart
+++ bin/soal_8.dart
@@ -1,20 +1,12 @@
 void main() {
   String yourText1 = 'ular';
-  String yourText2 = 'luar';
+  String yourText2 = 'alur';
 
-  // isAnagram(yourText1, yourText2);
-  print(isAnagram(yourText1, yourText2));
+  isAnagram(yourText1, yourText2);
+  // print(isAnagram(yourText1, yourText2));
 }
 
-bool isAnagram(String text1, String text2) {
-  List<String> sorted1 = text1.split("");
-  List<String> sorted2 = text2.split("");
-
-  sorted1.sort();
-  sorted2.sort();
-
-  if (sorted1 == sorted2) {
-    return true;
-  }
-  return false;
+void isAnagram(String text1, String text2) {
+  List<String> sorted1 = text1.split("")..sort();
+  List<String> sorted2 = text2.split("")..sort();
 }
